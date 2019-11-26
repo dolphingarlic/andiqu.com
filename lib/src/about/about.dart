02efcd6b05a498ecca7b19bbd876ef38d0f3815e
Andi Qu <dolphingarlic@gmail.com>
2019-11-26 12:37:14 +0200

    Updated "About"

--- lib/src/about/about.dart
+++ lib/src/about/about.dart
@@ -17,47 +17,66 @@ class AboutComponent {
 
   AboutComponent(this.sanitizer) {
     fun_about = this.sanitizer.bypassSecurityTrustHtml('''
-    <p>I am a dynamic figure, often seen creating fake
-        emails to get more free trials and preaching
-        Communism to Icelandic sheepherders. Sometimes,
-        if you're lucky, you'll even see me erecting massive 500
-        story skyscrapers in my backyard. When I am bored, I charm
+    <p>
+        I am a <b>dynamic figure</b>, often seen <b>creating fake
+        emails</b> to get more free trials and <b>preaching
+        Communism</b> to Icelandic sheepherders. Sometimes,
+        if you're lucky, you'll even see me erecting massive 500-story
+        skyscrapers in my backyard. When I am bored, I charm
         cats with my godlike yodelling skills and my
-        transcendental clarinet squeaks.</p>
+        transcendental clarinet squeaks.
+    </p>
   
-    <p>I am motivated to take risks. Sometimes, because I like
-        to live dangerously, I play chess with one less pawn
-        and do geometry in pen. Sometimes I even
+    <p>
+        I am <b>motivated to take risks</b>. Sometimes, because I like
+        to live dangerously, I play chess with <b>one less pawn</b>
+        and do <b>geometry in pen</b>. Sometimes I even
         play on the Wii without the safety strap! I know
         - I'm a real daredevil. Sometimes I'm even amazed myself
         that I've managed to survive this long in the first
-        place, what with all these totally dangerous and not
+        place, what with all these <b>totally dangerous</b> and not
         at all normal feats! But nothing quite beats the
         adrenaline rush that I get from participating in a class
-        discussion.</p>
+        discussion.
+    </p>
+
+    <p>
+        What many may find perplexing is St John's College's (frustrating) <b>inability to
+        spell my 4-letter name</b>. However, this is simply one of many testaments to the fact
+        that I simply <b>transcend the English language</b>. This is an elusive position
+        shared only by <b>literally every other language</b> (except Javascript).
+    </p>
+
+    <p>
+        Unfortunately, I cannot hack Facebook or fix your washing machine
+        using Scratch. However, given enough time, I can <b>solve imaginary problems</b>
+        concocted by <b>Polish mathematicians</b> with C++ or create <b>the most perfect but
+        disappointing Japanese flag</b> you have ever seen with the HTML canvas.
+        Sometimes, I even salute the planet with Python!
+    </p>
   
     <div style="text-align: center;">
         <p>Who am I? Of course, I am...</p>
         <b>
           The One<br>
           The Only<br>
-          Andi Qu
+          <h2>Andi Qu<h2>
         </b>
     </div>
     ''');
 
     boring_about = this.sanitizer.bypassSecurityTrustHtml('''
-    <p>Salutations, everyone. Andi here</p>
+    <p>Hi, everyone! Andi here</p>
     <p>
         I am a high school student from St John's College, Johannesburg. I started programming in grade 9, and since
         then it has <span style="text-decoration: line-through;">taken over my life</span> become one of my passions
-        and I have participated in quite a few contests since then
+        and I have participated in quite a few contests since then.
     </p>
     <p>
         My interests include playing the piano and clarinet, competitive programming, and olympiad maths.
-        Sometimes, I also (try to) contribute to FOSS initiatives like
+        Sometimes, I also contribute to FOSS initiatives like
         <a style="color: #409de6 !important;text-decoration: none;" href="https://www.apertium.org">Apertium</a>,
-        but unfortunately this does not happen very often
+        and I also enjoy participating in and mentoring for open-source competitions such as Hacktoberfest, the Google Code-In, and the Google Summer of Code.
     </p>
     <p>
         "But Andi", I hear you cry, "where are all the cool things you've coded up? Surely someone as <b>cool</b>
@@ -69,14 +88,13 @@ class AboutComponent {
         actually-useful software. But fear not! I have created/contributed towards some fun stuff like
         <a style="color: #409de6 !important;text-decoration: none;" href="https://dolphingarlic.github.io/Apertium-Global-PairViewer">the Apertium Global PairViewer</a> and
         <a style="color: #409de6 !important;text-decoration: none;" href="https://incredibowl.herokuapp.com">an <i>Incredibowl</i> website</a>.
-        I also made a
-        <a style="color: #409de6 !important;text-decoration: none;" href="https://assistant.google.com/services/a/uid/000000acca8b1c5d?hl=en">Google Assistant Action</a>!
+        Other projects can be found in the next section of this website!
         If you want, check out my
-        <a style="color: #409de6 !important;text-decoration: none;" href="https://dolphingarlic.github.io/useless_websites">list of useless websites I made</a>
+        <a style="color: #409de6 !important;text-decoration: none;" href="https://dolphingarlic.github.io/useless_websites">list of useless websites I made</a>.
     </p>
     <p>
         Unfortunately (or fortunately, depending on how much you despise my writing), I do not have a blog (yet),
-        so you'll have to trust that this website accurately conveys who I am
+        so you'll have to trust that this website accurately conveys who I am.
     </p>
     ''');
   }
