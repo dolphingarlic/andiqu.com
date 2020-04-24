30159932ec5aa1bacbad9f9d35d577880f79e3ff
dolphingarlic <dolphingarlic@gmail.com>
2020-04-24 20:06:22 +0200

    PWA

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ unzip dart.zip
 PUB=./dart-sdk/bin/pub
 ${PUB} global activate webdev
 ${PUB} get .
-${PUB} global run webdev build --output=web:build
+webdev build
 ${PUB} run pwa
-${PUB} global run webdev build --output=web:build
+webdev build
 find build -type f -name "*.js.map" -exec rm {} +
