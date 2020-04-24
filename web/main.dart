dccd556fa0076c3c00eee1eb4aa98757b6fc4afb
dolphingarlic <dolphingarlic@gmail.com>
2020-04-24 21:21:59 +0200

    PWA worker client

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,13 @@ import 'package:angular/angular.dart';
 import 'package:andiqu/andiqu.template.dart' as ng;
 import 'package:pwa/client.dart' as pwa;
 
+import 'main.template.dart' as self;
+
+@GenerateInjector([
+  ClassProvider(pwa.Client)
+])
+final InjectorFactory rootInjector = self.rootInjector$Injector;
+
 void main() {
-  new pwa.Client();
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory, createInjector: rootInjector);
 }
