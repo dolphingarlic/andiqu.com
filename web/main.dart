b5880c9c2d0bd304c84f234068b5860af6221a46
dolphingarlic <dolphingarlic@gmail.com>
2020-04-24 21:42:51 +0200

    Made PWA work offline

--- web/main.dart
+++ web/main.dart
@@ -2,13 +2,7 @@ import 'package:angular/angular.dart';
 import 'package:andiqu/andiqu.template.dart' as ng;
 import 'package:pwa/client.dart' as pwa;
 
-import 'main.template.dart' as self;
-
-@GenerateInjector([
-  ClassProvider(pwa.Client)
-])
-final InjectorFactory rootInjector = self.rootInjector$Injector;
-
 void main() {
-  runApp(ng.AppComponentNgFactory, createInjector: rootInjector);
+  runApp(ng.AppComponentNgFactory);
+  new pwa.Client();
 }
