0ad504e4eb4b2e0c58581ef610d16ed9f862fe67
dolphingarlic <dolphingarlic@gmail.com>
2020-04-25 12:03:07 +0200

    Changed start_url

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,6 @@ import 'package:andiqu/andiqu.template.dart' as ng;
 import 'package:pwa/client.dart' as pwa;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
   new pwa.Client();
+  runApp(ng.AppComponentNgFactory);
 }
