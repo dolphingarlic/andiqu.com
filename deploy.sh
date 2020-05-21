1a1cd434ad4f9ee4de20f76fdd9c3daae1ecc986
dolphingarlic <dolphingarlic@gmail.com>
2020-05-21 15:15:52 +0200

    Changed dart version

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-DART_VER='2.7.2'
+DART_VER='2.8.1'
 
 curl -o dart.zip https://storage.googleapis.com/dart-archive/channels/stable/release/${DART_VER}/sdk/dartsdk-linux-x64-release.zip
 unzip dart.zip
@@ -12,5 +12,6 @@ ${PUB} global activate webdev
 ${PUB} get .
 ${PUB} global run webdev build
 ${PUB} run pwa
+${PUB} get .
 ${PUB} global run webdev build
 find build -type f -name "*.js.map" -exec rm {} +
