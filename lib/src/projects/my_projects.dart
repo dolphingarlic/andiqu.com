fc671bce401e2d9dc4dc3f42ec0c1cd843e7bf82
Andi Qu <dolphingarlic@gmail.com>
2020-11-25 16:19:22 +0200

    Added Ruka to projects

--- lib/src/projects/my_projects.dart
+++ lib/src/projects/my_projects.dart
@@ -2,6 +2,13 @@ import 'project_card.dart';
 
 final List<Project> my_projects = [
   Project(
+    'Ruka',
+    'My first Unity game - draw on a canvas using a mechanical arm! "Ruka" ("Рука") is Russian/Bulgarian/Croatian for "the hand".',
+    'November 2020',
+    'https://dolphingarlic.itch.io/ruka',
+    'ruka.png'
+  ),
+  Project(
     'Guess the Tune!',
     'A website where you guess the source of a tune given its notes.',
     'November 2020',
@@ -37,66 +44,66 @@ final List<Project> my_projects = [
     'st0nks.png'
   ),
   Project(
-      'Five Streak',
-      'A website where you try to keep a streak to help prevent the spread of COVID-19.',
-      'May 2020',
-      'https://www.five-streak.com',
-      'five-streak.png'
+    'Five Streak',
+    'A website where you try to keep a streak to help prevent the spread of COVID-19.',
+    'May 2020',
+    'https://www.five-streak.com',
+    'five-streak.png'
   ),
   Project(
-      'Git the lines',
-      'A Discord bot that prints out the lines referenced in a code snippet link.',
-      'May 2020',
-      'https://github.com/dolphingarlic/git-the-lines',
-      'git-the-lines.png'
+    'Git the lines',
+    'A Discord bot that prints out the lines referenced in a code snippet link.',
+    'May 2020',
+    'https://github.com/dolphingarlic/git-the-lines',
+    'git-the-lines.png'
   ),
   Project(
-      'PO(I)TD',
-      'A Discord bot that sends a daily Olympiad Informatics problem.',
-      'April 2020',
-      'https://github.com/dolphingarlic/PO-I-TD',
-      'po-i-td.png'
+    'PO(I)TD',
+    'A Discord bot that sends a daily Olympiad Informatics problem.',
+    'April 2020',
+    'https://github.com/dolphingarlic/PO-I-TD',
+    'po-i-td.png'
+  ),
+  Project(
+    'APIOIPA',
+    'API for Olympiad Informatics Problems Assorted',
+    'April 2020',
+    'https://apioipa.herokuapp.com',
+    'apioipa.png'
   ),
   // Project(
-  //     'APIOIPA',
-  //     'API for Olympiad Informatics Problems Assorted',
-  //     'April 2020',
-  //     'https://apioipa.herokuapp.com',
-  //     'apioipa.png'
-  // ),
-  // Project(
-  //     'Polygrade',
-  //     'A website that lets the user input their grades and view it as a radar chart',
-  //     'December 2019',
-  //     'https://polygrade.herokuapp.com/',
-  //     'polygrade.png'
+  //   'Polygrade',
+  //   'A website that lets the user input their grades and view it as a radar chart',
+  //   'December 2019',
+  //   'https://polygrade.herokuapp.com/',
+  //   'polygrade.png'
   // ),
   Project(
-      'Joining Points',
-      'The game \'Joining Points\' from IOI 2006.',
-      'November 2019',
-      'https://github.com/dolphingarlic/joining_points',
-      'joining_points.png'
+    'Joining Points',
+    'The game "Joining Points" from IOI 2006.',
+    'November 2019',
+    'https://github.com/dolphingarlic/joining_points',
+    'joining_points.png'
   ),
   Project(
-      'SACO Editorials',
-      'A collection of editorials for the South African Programming Olympiad.',
-      'October 2019',
-      'https://github.com/dolphingarlic/SACO_editorials',
-      'github.png'
+    'SACO Editorials',
+    'A collection of editorials for the South African Programming Olympiad.',
+    'October 2019',
+    'https://github.com/dolphingarlic/SACO_editorials',
+    'github.png'
   ),
   Project(
-      'Incredibowl',
-      'A webiste that allows people to view, rate, and share bowls.',
-      'August 2019',
-      'https://incredibowl.herokuapp.com/',
-      'incredibowl.png'
+    'Incredibowl',
+    'A website that allows people to view, rate, and share bowls.',
+    'August 2019',
+    'https://incredibowl.herokuapp.com/',
+    'incredibowl.png'
   ),
   Project(
-      'Accomplice in Coding',
-      'A Google Assistant action that gives you random problems from informatics Olympiads.',
-      'July 2019',
-      'https://assistant.google.com/services/a/uid/000000acca8b1c5d?hl=en&source=web',
-      'ac_bot.png'
+    'Accomplice in Coding',
+    'A Google Assistant action that gives you random problems from informatics Olympiads.',
+    'July 2019',
+    'https://assistant.google.com/services/a/uid/000000acca8b1c5d?hl=en&source=web',
+    'ac_bot.png'
   ),
 ];
